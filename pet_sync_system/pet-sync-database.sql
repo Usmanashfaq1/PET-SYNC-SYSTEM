cea4e9faa3a5faeddc6d50b923f95b936b4c6052
UsmanAshfaq1 <f200115@cfd.nu.edu.pk>
2023-11-11 17:13:18 +0500

    This is the first commit its, contains : sign-in page, register user page, admin page, code verification pages(gmail), 20F-0115 Usman Ashfaq (i created these pages (frontend) -Validation of fields,20F-1088 Minahil Ali,helped  me with the front end pages. 20F-0116 M.SAMEED( Added the Backend functionality, api's on js file, created Database(my sql), also added bcrypt to encrypt for password encryption and database my sql configeration using mysql module of node.....   i (20F-0115 created the structure of this workspace

---
+++ pet_sync_system/pet-sync-database.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 10, 2023 at 06:03 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pet-sync-database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sign_up`
+--
+
+CREATE TABLE `sign_up` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(500) NOT NULL,
+  `confirm_password` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `sign_up`
+--
+
+INSERT INTO `sign_up` (`id`, `username`, `email`, `password`, `confirm_password`) VALUES
+(1, 'sameed', 'sameed@gmail.com', '$2b$10$1ru/cZBX3oUzuXG0xrt38O3Z.wK.Sz811d4s.J8yvoAINtIzq8yfS', '$2b$10$1ru/cZBX3oUzuXG0xrt38O3Z.wK.Sz811d4s.J8yvoAINtIzq8yfS');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `sign_up`
+--
+ALTER TABLE `sign_up`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `sign_up`
+--
+ALTER TABLE `sign_up`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ pet_sync_system/pet-sync_backend.js
@@ -0,0 +1,121 @@
+var express = require("express");
+var cors = require("cors");
+const bcrypt = require("bcrypt");
+const nodemailer = require("nodemailer");
+
+var app = express();
+
+app.use(cors());
+
+const multer = require("multer");
+
+var mysql = require("mysql");
+
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.set("view engine", "ejs");
+
+var conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "pet-sync-database",
+});
+
+conn.connect(function (err) {
+  if (err) throw err;
+
+  console.log("Connection Sucessful");
+});
+
+
+//const salt = bcrypt.genSaltSync(10);
+
+app.get("/", function (req, res) {
+  res.render("insert");
+});
+
+app.post("/insert_sign_up", function (req, res) {
+  var username = req.body.username;
+  var email = req.body.email;
+  var password = req.body.password;
+  //var confirm_password = req.body.confirm_password;
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt);
+  var sql = `insert into sign_up(username,email,password,confirm_password) values('${username}', '${email}', '${hash.toString()}', '${hash.toString()}')`;
+
+  conn.query(sql, function (err, results) {
+    if (err) throw err;
+    else
+     res.json(1);
+  });
+});
+
+
+
+
+
+app.post("/unique_check_sign_up", function (req, res) {
+  var username = req.body.username;
+  var email = req.body.email;
+  var sql = `SELECT * FROM sign_up WHERE email= '${email}' || username = '${username}'`;
+  conn.query(sql, function (err2, results) {
+    if (err2) {
+      console.error(err2);
+    } else {
+        var found = false;
+        results.forEach(element => {
+          if (email == element.email && found == false) {
+            found = true;
+          }
+          else if (username == element.username && found == false) {
+            found = true;
+          }
+        });
+        if (found == true) {
+          res.json(1);
+        }
+        else {
+          res.json(-1);
+        }
+    }
+
+  });
+});
+
+app.post("/login", function (req, res) {
+
+  var password = req.body.password;
+  var email = req.body.email;
+  var sql = `SELECT password FROM sign_up WHERE email= '${email}'`;
+  conn.query(sql, function (err2, results) {
+    if (err2) {
+      console.error(err2);
+    } else {
+      var found = false;
+      results.forEach(element => {
+        if (bcrypt.compareSync(password.toString(), element.password) && found == false) {
+          found = true;
+        }
+      });
+      if (found == true) {
+        res.json(1);
+      }
+      else {
+        res.json(-1);
+      }
+    }
+
+  });
+});
+
+
+
+var server = app.listen(4000, function () {
+  console.log("App running on port 4000");
+});
+
+
