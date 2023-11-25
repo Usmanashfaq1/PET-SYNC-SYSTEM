1708f2ef519030e79a7a8af4b4bc1da1f989e13e
sameedhere123 <f200116@cfd.nu.edu.pk>
2023-11-25 10:52:21 +0500

    Added ajax call and Backend API for view Pet profile bypassing id through the prams

--- pet_sync_system_updated/pet-sync_backend.js
+++ pet_sync_system_updated/pet-sync_backend.js
@@ -112,7 +112,45 @@ app.get('/get_profiles/:username', (req, res) => {
   });
 });
 
+app.get('/open_profile/:id', (req, res) => {
+  const id = req.params.id;
+  const sql = `SELECT * FROM pet_profile WHERE id = '${id}'`;
 
+  conn.query(sql, (err, result) => {
+    if (err) {
+      console.error('Error fetching pet profiles:', err);
+      res.status(500).json({ error: 'An error occurred while fetching pet profiles' });
+      return;
+    }
+
+
+    const fileContents = [];
+
+    // Assuming 'petPicture' is the column name
+    result.forEach(row => {
+      const fileName = row.petPicture;
+
+      // Check if fileName is defined
+      if (fileName) {
+        const filePath = path.join(__dirname, 'upload', fileName);
+
+        try {
+          // Read the file content in base64
+          const content = fs.readFileSync(filePath, { encoding: 'base64' });
+
+          // Include all values from the result set along with the file content
+          const rowWithFileContent = { ...row, petPicture: content };
+          fileContents.push(rowWithFileContent);
+        } catch (err) {
+          console.error('Error reading file:', err);
+        }
+      }
+    });
+
+    // Send the array of result and file contents as a response
+    res.json(fileContents);
+  });
+});
 
 
 app.get("/", function (req, res) {
