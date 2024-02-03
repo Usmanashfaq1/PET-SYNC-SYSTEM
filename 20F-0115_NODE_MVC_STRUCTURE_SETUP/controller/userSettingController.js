84f520b3f6752577298eec72219b4b60f9a683f6
Usmanashfaq1 <f200115@cdf.nu.edu.pk>
2024-02-03 12:20:28 +0500

    Added User Profile setting Controller rendering userSetting.ejs with payload accordingly and added routes and navigation accordingly

---
+++ 20F-0115_NODE_MVC_STRUCTURE_SETUP/controller/userSettingController.js
@@ -0,0 +1,33 @@
+const connection = require('../config');
+const Model = require('../model/editProfileModel.js');
+const editProfileModel = new Model();
+
+class editProfileController{
+    displayPage (req,res){
+        //console.log(req.session.num);
+        if(req.session.num!=null && typeof req.session.num!="undefined"){//If user has logged in
+            console.log("LoggedIn "+req.session.num);
+            var name=req.session.username;
+            let userInfoQuery="SELECT * FROM userinfo WHERE id='"+req.session.num+"'";
+                    connection.query(userInfoQuery,(error,userInfoResult)=>{
+                        if(error) throw error;
+                        console.log(userInfoResult[0]);
+                        return res.render('userSetting',{msg:"null",userInfo:userInfoResult[0],name});
+                    });
+        }
+        else{ 
+            console.log("Not loggedIn "+req.session.num);
+            return res.redirect('/login');
+        }
+    }
+    
+    enterInfo (req,res){
+        editProfileModel.enterInfo(req,res);
+        //console.log("contMsg=> "+msg);
+    } 
+    
+}
+module.exports = editProfileController;
+
+ 
+
