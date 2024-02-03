9428a196010f824a3a32d3e1deacf39c1037580a
Usmanashfaq1 <f200115@cdf.nu.edu.pk>
2024-02-03 12:17:15 +0500

    Added User Profile setting data logic created profile model and upload folder for storing pictures

---
+++ 20F-0115_NODE_MVC_STRUCTURE_SETUP/model/editProfileModel.js
@@ -0,0 +1,39 @@
+const connection = require('../config');
+
+
+class editProfileModel {
+    
+    enterInfo(req,res){
+        console.log(req.file);
+        console.log(req.body);
+
+        connection.query('SELECT * FROM userinfo WHERE id=?',req.session.num,(error,userInfoResult)=>{
+        if(error) throw error;
+            var userInfo = {
+                "username":(req.body.userName=='')?userInfoResult[0].username:req.body.userName,
+                "profilepic":(!req.file)?userInfoResult[0].profilepic:req.file.filename,
+                "fullname":(req.body.fullName=='')?userInfoResult[0].fullname:req.body.fullName,
+                "birthdate":(req.body.DOB=='')?userInfoResult[0].birthdate:req.body.DOB,
+                "bio":(req.body.Bio=='')?userInfoResult[0].bio:req.body.Bio,
+            };
+        
+            var sqlQuery1="UPDATE userinfo SET ? WHERE id="+req.session.num;
+            var sqlQuery2="UPDATE users SET username=? WHERE id="+req.session.num;
+            
+            connection.query(sqlQuery2,userInfo.username,(error,reslt)=>{
+                if(error) throw error;
+                connection.query(sqlQuery1,userInfo,(error,result)=>{
+                    if(error) throw error;
+                    let userInfoQuery="SELECT * FROM userinfo WHERE id='"+req.session.num+"'";
+                    connection.query(userInfoQuery,(error,userInfoResult)=>{
+                        if(error) throw error;
+                        console.log(userInfoResult[0]);
+                        return res.render('editProfileView',{msg:"Saved successfully!",userInfo:userInfoResult[0]});
+                    });
+                });
+            });
+        });
+    }
+}
+
+module.exports = editProfileModel;
