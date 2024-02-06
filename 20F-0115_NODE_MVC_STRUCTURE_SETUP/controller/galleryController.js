28068279f5a79de80b32d283f2008cd412fe2853
Usmanashfaq1 <f200115@cdf.nu.edu.pk>
2024-02-06 10:10:03 +0500

    Added Controller logic for gallery view and added routes for it too.. added display page function to display feed results from table for current user session

---
+++ 20F-0115_NODE_MVC_STRUCTURE_SETUP/controller/galleryController.js
@@ -0,0 +1,50 @@
+const profileModel = require('../model/profileModel');
+const GalleryModel=require('../model/galleryModel');
+
+
+class GalleryController {
+    async displayPage(req, res) {
+        try {
+            if (req.session.num != null && typeof req.session.num != "undefined")
+             {
+                console.log("LoggedIn " + req.session.num);
+                var name=req.session.username;
+                const userInfo = await profileModel.getUserInfo(req.session.num);
+                let imageName = userInfo[0].profilepic;
+                
+
+                if (typeof imageName == 'object')
+                 {
+                    imageName = "default_profilepic.png";
+                 }
+
+                 
+                 const feedResult = await GalleryModel.getUserFeed(req.session.num);
+                return res.render('gallery', { data: userInfo[0], feedResult: feedResult,dpName: imageName, name });
+            } 
+            else 
+            {
+                console.log("Not loggedIn " + req.session.num);
+                return res.redirect('/login');
+            }
+        } 
+        catch (error)
+         {
+            console.error(error);
+            return res.status(500).send('Internal Server Error');
+        }
+    }
+
+    async enterInfo(req, res) {
+        try {
+            // Handle entering user info
+        } catch (error) {
+            console.error(error);
+            return res.status(500).send('Internal Server Error');
+        }
+    }
+
+    
+}
+
+module.exports = GalleryController;
