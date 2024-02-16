7da6e81ac48a7c71b6d27020dc6070d116bea483
Sameedhere123 <f200116@cfd.nu.edu.pk>
2024-02-16 11:49:03 +0500

    Sameed 2024 Branch for E-commerce module

---
+++ petsync_sameed_2024/routes/route.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const router = express.Router();
+
+
+const { load_products_page } = require('../controller/E-commerce/products')
+const { load_admin_page } = require('../controller/E-commerce/admin')
+const { load_admin_products_page } = require('../controller/E-commerce/admin')
+const { load_edit_product_page } = require('../controller/E-commerce/edit_products')
+const { load_update_product_page } = require('../controller/E-commerce/edit_products')
+const { load_open_cart_page } = require('../controller/E-commerce/cart')
+
+
+
+
+const { handle_add_product } = require('../controller/E-commerce/admin')
+const { handle_get_products } = require('../controller/E-commerce/products')
+const { handle_delete_product } = require('../controller/E-commerce/edit_products')
+const { handle_get_specific_product } = require('../controller/E-commerce/edit_products')
+const { handle_load_update_product } = require('../controller/E-commerce/edit_products')
+const { handle_updated_product_data } = require('../controller/E-commerce/edit_products')
+const { handle_add_to_cart } = require('../controller/E-commerce/cart')
+const { handle_cart_item_number } = require('../controller/E-commerce/cart')
+const { handle_get_cart_items } = require('../controller/E-commerce/cart')
+const { handle_remove_from_cart } = require('../controller/E-commerce/cart')
+
+
+
+
+router.get('/update_product', load_update_product_page);
+
+router.get('/open_cart_page', load_open_cart_page);
+
+
+router.get('/get_products', handle_get_products);
+
+router.get('/get_specific_product/:category', handle_get_specific_product);
+
+router.get('/E-commerce', load_products_page);
+
+router.get('/admin_dashboard', load_admin_page);
+
+router.get('/admin_products', load_admin_products_page);
+
+router.post('/add_product', handle_add_product);
+
+router.get('/edit_product', load_edit_product_page);
+
+router.delete('/delete_product/:id', handle_delete_product);
+
+router.get('/load_update_product/:id', handle_load_update_product);
+
+router.post('/updated_product_data', handle_updated_product_data);
+
+router.post('/add_to_cart', handle_add_to_cart);
+
+router.get('/cart_item_number', handle_cart_item_number);
+
+router.get('/get_cart_items', handle_get_cart_items);
+
+router.delete('/remove_from_cart/:id', handle_remove_from_cart);
+
+
+module.exports = router;
